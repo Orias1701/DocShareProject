571b0c93efbf9bf425973c4f4e487d4d7bb69df0
thangnd05 <thangnd.contact@gmail.com>
2025-09-03 14:25:01 +0700

    mess

--- views/reaction/reaction_bar.php
+++ views/reaction/reaction_bar.php
@@ -1,10 +1,10 @@
 <div class="reaction-bar">
-<?php foreach ($reactions as $type => $icon): 
-    $active = (isset($userReaction) && $userReaction === $type) ? "active" : "";
-    $count = isset($reactionCounts[$type]) ? $reactionCounts[$type] : 0;
-?>
-    <a href="index.php?action=toggle_reaction&reaction_type=<?php echo $type; ?>&post_id=<?php echo $post['post_id']; ?>" class="<?php echo $active; ?>">
-        <?php echo $icon . " " . $count; ?>
-    </a>
-<?php endforeach; ?>
+    <?php foreach ($reactions as $type => $icon):
+        $active = (isset($userReaction) && $userReaction === $type) ? "active" : "";
+        $count = isset($reactionCounts[$type]) ? $reactionCounts[$type] : 0;
+    ?>
+        <a href="index.php?action=toggle_reaction&reaction_type=<?php echo $type; ?>&post_id=<?php echo $post['post_id']; ?>" class="<?php echo $active; ?>">
+            <?php echo $icon . " " . $count; ?>
+        </a>
+    <?php endforeach; ?>
 </div>
